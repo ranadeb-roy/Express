064b9730044221c0d9436ebac2a097096f9407e2
Ranadeb Roy <ranadebroy.rr2005@gmail.com>
2026-09-09 22:56:24 +0530

    sent a GET request to the root endpoint

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const express = require('express');
 // like Scanner sc = new Scanner(System.in); -> (object initialization)
 const app = express();
 
+app.get('/', (req, res) => {
+    res.status(200).json({ 
+        message: 'Welcome to Express JS' 
+    });
+});
+
 app.listen(3000, () => {
     console.log('Server is running on 3000 port.');
-    console.log('Hello World');
-})
+});
